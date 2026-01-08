d9a7f02d9953c8fc773eb1c016ceb3297f2ee49b
Chetan Bairagi <your-email@example.com>
2026-01-08 16:44:43 +0530

    Phase: initial app backend setup

---
+++ app/backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();   // 👈 THIS WAS MISSING
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Backend is running 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
