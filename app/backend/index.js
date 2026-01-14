7c2509e196bd3392efc7ca9339c31508ec680d6b
Chetan Bairagi <your-email@example.com>
2026-01-14 18:18:14 +0530

    dfdfdd

--- app/backend/index.js
+++ app/backend/index.js
@@ -3,12 +3,3 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Basic route
-app.get('/', (req, res) => {
-  resfdfddfdfdfdfd.send('Oops!');
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
